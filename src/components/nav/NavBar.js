7b20ab954bb6098409a4c9c6fe4694357a31fa98
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-08-30 22:27:09 -0400

    its looking pretty with candy colors and nav bar looks more like a nav bar. using grid instead of flex, lets not tell others about it

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,25 +3,23 @@ import "./NavBar.css"
 
 export const NavBar = () => {
     const navigate = useNavigate()
-    
-    const localKandyUser = localStorage.getItem('kandy_user')
-    const kandyUserObject = JSON.parse(localKandyUser) 
 
-//!WHAT AM I MISSING HERE?!
+    const localKandyUser = localStorage.getItem('kandy_user')
+    const kandyUserObject = JSON.parse(localKandyUser)
+
+    //!WHAT AM I MISSING HERE?!
+    //?This is using a ternary statement to show the all products options only to staff members
     return (<>
         <ul className="navbar">
             <li className="navbar__item active">
                 <Link className="navbar__link" to="/locations">Locations</Link>
             </li>
-            //?This is using a ternary statement to show the all products options only to staff members
             {kandyUserObject.staff ?
-                    <li className="navbar__item active">
-                        <Link className="navbar__link" to="/products">All Products</Link>
-                    </li>
-                :<li></li>
+                <li className="navbar__item active">
+                    <Link className="navbar__link" to="/products">All Products</Link>
+                </li>
+                : <li></li>
             }
-        
-
             <li className="navbar__item navbar__logout">
                 <Link className="navbar__link" to="" onClick={() => {
                     localStorage.removeItem("kandy_user")
--- src/components/products/products.js
+++ src/components/products/products.js
@@ -4,7 +4,33 @@ import "./products.css"
 export const ProductsList = () => {
     const [products, setProducts] = useState([])
 
+    useEffect(() => {
+        fetch('http://localhost:8088/products')
+            .then(response => response.json())
+            .then((productsArray) => {
+                setProducts(productsArray)
+            })
+    },
+        [])
 
 
-//this is closing the ProductList function    
+    return <>
+        <h1>List of Products</h1>
+        <article className="productsList">
+            {
+                products.map(product => {
+                    return <>
+                        <div className="eachProduct">
+                            <h3>{product.name}</h3>
+                            Costs:$USD{product.price}
+                            <br></br>
+                            Available:{product.productQuantity}
+                        </div>
+                    </>
+                })
+            }
+        </article>
+    </>
+
+    //this is closing the ProductList function    
 }
