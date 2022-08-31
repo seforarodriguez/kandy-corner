f1a5e7dfbe8ce65dadfe83c54d28cee5f0f39d51
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-08-30 20:05:01 -0400

    trying to setup the products button to show up with a ternary statement

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,20 +3,36 @@ import "./NavBar.css"
 
 export const NavBar = () => {
     const navigate = useNavigate()
+    
+    const localKandyUser = localStorage.getItem('kandy_user')
+    const kandyUserObject = JSON.parse(localKandyUser) 
 
-    return (
+//!WHAT AM I MISSING HERE?!
+    return (<>
         <ul className="navbar">
             <li className="navbar__item active">
                 <Link className="navbar__link" to="/locations">Locations</Link>
             </li>
+            {
+                kandyUserObject.isStaff 
+                ? <>
+                    <li className="navbar__item active">
+                        <Link className="navbar__link" to="/products">All Products</Link>
+                    </li>
+                </>
+                : ""  
+            }
+            
+            
 
             <li className="navbar__item navbar__logout">
                 <Link className="navbar__link" to="" onClick={() => {
                     localStorage.removeItem("kandy_user")
-                    navigate("/", {replace: true})
+                    navigate("/", { replace: true })
                 }}>Logout</Link>
             </li>
         </ul>
+    </>
     )
 }
 
---
+++ src/components/products/products.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+import "./products.css"
+
+export const ProductsList = () => {
+    const [products, setProducts] = useState([])
+
+
+
+//this is closing the ProductList function    
+}
