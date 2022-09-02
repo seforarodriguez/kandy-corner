2c5211c21c5d3e8c4bbe267fe8772ceb155241dc
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-09-02 15:55:17 -0400

    button was missing, and got aadded

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -3,22 +3,38 @@ import "./products.css"
 
 export const ProductsList = () => {
     const [products, setProducts] = useState([])
+    const [filteredProducts, setFiltered] = useState([])
+    const [topPricedProducts, setTopPriced] = useState(false)
+
 
     useEffect(() => {
         fetch('http://localhost:8088/products')
             .then(response => response.json())
             .then((productsArray) => {
                 setProducts(productsArray)
+                setFiltered(productsArray)
             })
     },
         [])
 
+        useEffect(()=> {
+            if (topPricedProducts) {
+                const topPricedArray = products.filter(product => {
+                    return product.price > 2
+                })
+            setFiltered(topPricedArray)
+            } else {
+                setFiltered(products)
+            }
+        },[topPricedProducts])
 
     return <>
+
         <h1>List of Products</h1>
+        <button onClick={()=> { topPricedPictures ?setTopPriced(true) :setFiltered(false)}}>Top Priced Products</button>
         <article className="productsList">
             {
-                products.map(product => {
+                filteredProducts.map(product => {
                     return <>
                         <div className="eachProduct">
                             <h3>{product.name}</h3>
