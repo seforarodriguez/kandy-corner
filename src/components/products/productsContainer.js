e90a3d97a104aff32674a157af2ba6eece8d1111
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-09-09 10:36:49 -0400

    filtering is working correctly

---
+++ src/components/products/productsContainer.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import { ProductsList } from "./products"
+import { ProductSearch } from "./ProductSearch"
+
+export const ProductContainer = () => {
+    const [searchProducts, setSearchProducts] = useState("")
+
+    return <>
+    {/* Using this route to call from here the 2 functions from the different components */}
+        <ProductSearch searchFunction={setSearchProducts}/>
+        <ProductsList theProductSearch={searchProducts}/>
+    </>
+
+} //closing bracket product container
